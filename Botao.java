1e4cd437d9301566e8b7d5dd261bc04b9a83b836
Bryann Barbosa <bryannbarbosa@gmail.com>
2016-11-20 00:00:59 -0200

    Update Botao.java

--- Botao.java
+++ Botao.java
@@ -1,15 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package botao;
 
 import java.util.Scanner;
 
 /**
  *
- * @author Bryann
+ * @author Bryann Barbosa
  */
 public class Botao {
 
